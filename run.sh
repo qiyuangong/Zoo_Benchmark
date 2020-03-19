a433f3f266693cb9090ce3b063aa524e9027cd54
Qiyuan Gong <qiyuan.gong@intel.com>
2020-03-19 17:19:05 +0800

    Refine bash script

--- run.sh
+++ run.sh
@@ -3,18 +3,24 @@
 # debug flag
 #set -x
 
-HYPER=1
-CPU=$(($(nproc) / HYPER))
-echo "Core number ${CPU}"
+HYPER=`lscpu |grep "Thread(s) per core"|awk '{print $4}'`
+CORES=$(($(nproc) / HYPER))
+
+if [[ -z "${KMP_AFFINITY}" ]]; then
+  KMP_AFFINITY=granularity=fine,compact
+  if [[HYPER==2]]; then
+    KMP_AFFINITY=${KMP_AFFINITY},1,0
+  fi
+fi
+echo "Hardware Core number ${CORES}"
 
 usage()
 {
     echo "usage:
-       1. type, tf, torch, bigdl or ov, e,g., bigdl
+       1. type, tf, torch, bigdl, bigdlblas or ov, e,g., bigdl
        2. Model path, e.g., *.model, *.xml
        3. Iteration, e.g., 100
        4. Batch Size, e.g., 32
-       5. blas, turn on mklblas
        as parameters in order. More concretely, you can run this command:
        bash run.sh \\
             bigdl \\
@@ -23,6 +29,10 @@ usage()
             32"
     exit 1
 }
+
+
+OPTIONS=""
+PARM=""
 
 if [ "$#" -lt 4 ]
 then
@@ -34,54 +44,47 @@ else
     BS="$4"
 fi
 
-if [ "$#" -gt 4 ]
-then
-    BACKEND="$5"
-fi
-
 case $TYPE in
 
   "tf" | "TF")
-    CLASS=com.intel.analytics.zoo.example.inference.TFNetPerf
+    echo "Analytics-Zoo with TensorFlow"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.TFNetPerf
     ;;
 
   "torch" | "TORCH")
-    CLASS=com.intel.analytics.zoo.example.inference.TorchNetPerf
-    OPTIONS='-Dbigdl.engineType=mkldnn -Dbigdl.mklNumThreads='${CPU}
-    export OMP_NUM_THREADS=${CPU}
+    echo "Analytics-Zoo with PyTorch"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.TorchNetPerf
+    export OMP_NUM_THREADS=${CORES}
     ;;
 
   "bigdl" | "BIGDL")
-    CLASS=com.intel.analytics.zoo.example.inference.BigDLPerf
-    OPTIONS='-Dbigdl.engineType=mkldnn -Dbigdl.mklNumThreads='${CPU}
+    echo "Analytics-Zoo with BigDL MKLDNN"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.BigDLPerf
+    OPTIONS='-Dbigdl.engineType=mkldnn -Dbigdl.mklNumThreads='${CORES}
     ;;
 
   "bigdlblas" | "BIGDLBLAS")
-    CLASS=com.intel.analytics.zoo.example.inference.BigDLBLASPerf
-    PARM="-c ${CPU}"
+    echo "Analytics-Zoo with BigDL BLAS"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.BigDLBLASPerf
+    PARM="-c ${CORES}"
     ;;
 
   "ov" | "OV")
-    CLASS=com.intel.analytics.zoo.example.inference.OpenVINOPerf
-    export OMP_NUM_THREADS=${CPU}
+    echo "Analytics-Zoo with OpenVINO"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.OpenVINOPerf
+    export OMP_NUM_THREADS=${CORES}
     export KMP_BLOCKTIME=20
     ;;
 
   *)
-    CLASS=com.intel.analytics.zoo.example.inference.BigDLPerf
-    OPTIONS='-Dbigdl.engineType=mkldnn -Dbigdl.mklNumThreads='${CPU}
+    echo "Analytics-Zoo with BigDL MKLDNN"
+    CLASS=com.intel.analytics.zoo.benchmark.inference.BigDLPerf
+    OPTIONS='-Dbigdl.engineType=mkldnn -Dbigdl.mklNumThreads='${CORES}
     ;;
 esac
 
-if [[ -z "${BACKEND}" ]]; then
-    echo "Using mkldnn"
-else
-    echo "Using mklblas"
-    OPTIONS=""
-fi
-
 
 # for maven
-JAR=target/benchmark-0.2.0-SNAPSHOT-jar-with-dependencies.jar
+JAR=target/benchmark-0.1.0-SNAPSHOT-jar-with-dependencies.jar
 
 java ${OPTIONS} -cp ${JAR} ${CLASS} -m ${MODEL} --iteration ${ITER} --batchSize ${BS} ${PARM}
